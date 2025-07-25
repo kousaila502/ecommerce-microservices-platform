af6e5a9bd029d9e3b208f79432896b6e2b45e75e
kousaila502 <k.benhamouche@esi-sba.dz>
2025-07-25 21:02:27 +0200

    Test GitOps pipeline automation

--- .history/users-cna-microservice/app_20250725210203.py
+++ .history/users-cna-microservice/app_20250725210203.py
@@ -49,7 +49,7 @@ async def health_check():
     return {
         "status": "healthy",
         "service": "user-service",
-        "version": "7.0",
+        "version": "7",
         "features": [
             "authentication",
             "user-management", 
